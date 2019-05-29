ccae88d912e4472583402058ad73ce39fde8c89b
Kévin <cortacero.k31130@gmail.com>
2019-05-29 15:25:13 +0200

    body is also working!

--- scripts/body_viz.py
+++ scripts/body_viz.py
@@ -12,6 +12,13 @@ from visualization_msgs.msg import MarkerArray, Marker
 
 from jsk_recognition_msgs.msg import PeoplePoseArray
 
+# Color map from tf_poses, TODO: param it with yaml
+from tf_pose.common import CocoColors
+
+COLOR_MAP = {}
+for i, c in enumerate(CocoColors):
+    COLOR_MAP[i] = ColorRGBA(r=c[0]/255.0, g=c[1]/255.0, b=c[2]/255.0, a=1.0)
+
 class BodyViz(object):
 
     def __init__(self, body_topic_sub, body_topic_pub):
@@ -27,8 +34,7 @@ class BodyViz(object):
             for limb_name, limb_pose in zip(person.limb_names, person.poses):
                 person_id, limb_id = limb_name.split(':')
                 unique_id = int(person_id)*100 + int(limb_id)
-                
-                m = Marker(pose=limb_pose, id=unique_id, type=Marker.SPHERE, action=Marker.ADD, scale=Vector3(x=0.1, y=0.1, z=0.1), color=ColorRGBA(r=1, a=1.0), lifetime=rospy.Time(1))
+                m = Marker(pose=limb_pose, id=unique_id, type=Marker.SPHERE, action=Marker.ADD, scale=Vector3(x=0.1, y=0.1, z=0.1), color=COLOR_MAP[int(limb_id)], lifetime=rospy.Time(1))
                 m.header.frame_id = people_pose_array.header.frame_id
                 m.header.stamp = people_pose_array.header.stamp
                 markers.append(m)
